ba3aee68ccf15d5f3d753f18fbfd690a8e6f63ac
daeun084 <030804jk@naver.com>
2025-01-19 00:50:13 +0900

    !hotfix: security 제거

--- src/main/java/com/backend/withFesta/common/status/SuccessStatus.java
+++ src/main/java/com/backend/withFesta/common/status/SuccessStatus.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum SuccessStatus implements BaseSuccessStatus {
 
     // 전역
-    OK(HttpStatus.OK, "S001", "요청이 성공적으로 처리되었습니다."),
-    CREATED(HttpStatus.CREATED, "S002", "리소스가 성공적으로 생성되었습니다.");
+    OK(HttpStatus.OK, "200", "요청이 성공적으로 처리되었습니다."),
+    CREATED(HttpStatus.CREATED, "200", "리소스가 성공적으로 생성되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
