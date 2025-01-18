ceb9abd958c79a740798ed5940269cb773f3ed03
heehoonhong <gmlgns0522@naver.com>
2025-01-18 19:29:25 +0900

    initial commit for backend

---
+++ src/main/java/com/backend/withFesta/WithFestaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.withFesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithFestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithFestaApplication.class, args);
+	}
+
+}
